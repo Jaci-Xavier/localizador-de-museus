437e6f6ee4bf7b1fa2d1d23b494d4a55d4e899e6
Jaci-Xavier <devxvr@gmail.com>
2023-11-09 16:01:03 -0300

    req 04

---
+++ src/main/java/com/betrybe/museumfinder/exception/MuseumNotFoundException.java
@@ -0,0 +1,10 @@
+package com.betrybe.museumfinder.exception;
+
+/**
+ * cria uma exceção unchecked por invalida coordenada.
+ *
+ */
+
+public class MuseumNotFoundException extends RuntimeException{
+
+}
